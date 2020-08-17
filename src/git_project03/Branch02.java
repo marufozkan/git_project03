8e409fe090c0edff47a2ef9a318cd19641e2c41e
maruf <mrfozkan47@gmail.com>
2020-08-17 17:06:38 +0300

    marif commit

--- src/git_project03/Branch02.java
+++ src/git_project03/Branch02.java
@@ -3,7 +3,9 @@ package git_project03;
 public class Branch02 {
 
 	public static void main(String[] args) {
+		
 		System.out.println("saat 16:58   branch ogreniyorum");
+		System.out.println("saat 17:05 maruf branch");
 
 	}
 
